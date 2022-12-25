f8461df9223114a9b052342dd70f76f6407d0fa1
nizar <nizar.dev01@gmail.com>
2022-12-26 03:26:04 +0530

    Configured Django Admin for the new user model.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,6 +7,7 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
     """Test models."""
 
+
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
         email = "test@example.com"
@@ -20,6 +21,7 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password)),
 
+
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
         sample_emails = [
@@ -31,3 +33,19 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sampl123')
             self.assertEqual(user.email, expected)
+
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            email = 'test@example.com',
+            password = 'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
